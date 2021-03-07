ce8b9ae736c3872f15403da7354f7a2b0058728a
Nabhy <nabhymishraalld@gmail.com>
2021-03-07 16:39:47 +0530

    platorm added

--- sketch.js
+++ sketch.js
@@ -5,16 +5,15 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-
+var platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
-
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    platform=new Ground(150,300,300,170);
     
     ground = new Ground(600,height,1200,20)
 
@@ -57,6 +56,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platform.display();
     bird.display();
 }
